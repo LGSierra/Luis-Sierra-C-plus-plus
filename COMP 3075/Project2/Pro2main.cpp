6588565cd3c3673885f9364f648b027fbf2370c6
Luis Sierra <37555594+LGSierra@users.noreply.github.com>
2018-09-03 01:57:15 -0400

    small fix on the exit

--- COMP 3075/Project2/Pro2main.cpp
+++ COMP 3075/Project2/Pro2main.cpp
@@ -11,7 +11,7 @@ using namespace CircleNamespace;
 	-Linked list structure
 	-Function definitions
 	-Object declaration
-	
+
 	PROGRAM NOT COMPLETE
 
 */
@@ -24,9 +24,9 @@ int main()
 	circulo object;
 	int choice;
 	do{
-		
+
 		choice = menu();
-		
+
 		switch(choice){
 			case 1:
 				object.capturarCirculo(max);
@@ -35,68 +35,68 @@ int main()
 				if(max == 0){
 					cout << "Lista esta vacia!" << endl;
 				}
-				
+
 				else{
-					
+
 					int selection; //selected node
 					//print linked list with coordinates and radius
 					object.printList(max);
 					if(max==1){
 						selection = 1;
 					}
-					
+
 					else{
 						cout << "Seleccione un circulo de la lista a ser calculado:" << endl;
 						cin >> selection;//something to validate later
 					}
-					
+
 					if(selection <=0 || selection > max){
 						cout << "seleccion invalida." << endl;
 					}
 					else{
-						
+
 						//choose circle function here
 						int selectX=0, selectY=0;
 						double selectR=0.0;
-						
+
 						object.selectExtract(max, selection, selectX, selectY, selectR);
-						
+
 						//if there is only one node, select by default
 						//create a temp object and fill it up with constructor
-						
+
 						circulo temp(selectX, selectY, selectR);
-						
+
 						cout.setf(ios::fixed);
 						cout.setf(ios::showpoint);
 						cout.precision(3);
-						
+
 						cout << "El area del circulo es: " << temp.obtenerArea() << " unidades cuadradas." << endl;
 						//object.obtenerArea();
 					}
-					
+
 				}
 				break;
-				
-				
+
+
 			case 3:
 				if(max == 0){
 					cout << "Lista esta vacia!" << endl;
 				}
-				
+
 				else{
-					
+
 					int selection; //selected node
 					//print linked list with coordinates and radius
 					object.printList(max);
 					if(max==1){
 						selection = 1;
 					}
-					
+
 					else{
 						cout << "Seleccione un circulo de la lista a ser calculado:" << endl;
 						cin >> selection;//something to validate later
 					}
-					
+
 					if(selection <=0 || selection > max){
 						cout << "seleccion invalida." << endl << endl;
 					}
@@ -104,19 +104,19 @@ int main()
 						//choose circle function here
 						int selectX=0, selectY=0;
 						double selectR=0.0;
-						
+
 						object.selectExtract(max, selection, selectX, selectY, selectR);
-						
+
 						//if there is only one node, select by default
 						//create a temp object and fill it up with constructor
-						
+
 						circulo temp(selectX, selectY, selectR);
-						
+
 						cout.setf(ios::fixed);
 						cout.setf(ios::showpoint);
 						cout.precision(3);
-						
-						
+
+
 						cout << "La circumsferencia del circulo es: " << temp.obtenerCircunsferencia() << " unidades." << endl;
 						//object.obtenerCircunsferencia();
 					}
@@ -126,80 +126,80 @@ int main()
 				if(max == 0){
 					cout << "Lista esta vacia!" << endl;
 				}
-				
+
 				else{
-					
+
 				int selection; //selected node
 				//print linked list with coordinates and radius
 				object.printList(max);
 				if(max==1){
 					selection = 1;
 				}
-				
+
 				else{
 					cout << "Seleccione un circulo de la lista a ser calculado:" << endl;
 					cin >> selection;//something to validate later
 				}
-					
+
 				if(selection <=0 || selection > max){
 						cout << "seleccion invalida." << endl << endl;
 				}
 				else{
-					
+
 					//choose circle function here
 					int selectX=0, selectY=0;
 					double selectR=0.0;
-					
+
 					object.selectExtract(max, selection, selectX, selectY, selectR);
-					
+
 					//if there is only one node, select by default
 					//create a temp object and fill it up with constructor
-					
+
 					circulo temp(selectX, selectY, selectR);
-					
+
 					cout.setf(ios::fixed);
 					cout.setf(ios::showpoint);
-					cout.precision(3);	
-					
+					cout.precision(3);
+
 					cout << "El diametro del circulo es " << temp.obtenerDiametro() << " unidades." << endl;
 				}
 				}
 				break;
-				
-				
+
+
 			case 5:
 				if(max == 0){
 					cout << "Lista esta vacia!" << endl;
 				}
-				
+
 				else{
-					object.printList2(max);				
+					object.printList2(max);
 				}
-				
+
 				//some function to loop void circulo::imprimirCirculo()
 				//maybe use a for loop? to print out the whole linked list
-				
+
 				break;
 			case 6:
 				if(max == 0){
 					cout << "Lista esta vacia!" << endl;
 				}
-				
+
 				else{
 					object.imprimirCirculo(max);
 				}
 				// same as case 5 but using void punto::imprimir()  m a y b e
 				break;
 			default:
-				if(choice!=0){
-					cout << "opcion invalida, intente de nuevo" << endl;
+				if(choice==0){
+					//cout << "opcion invalida, intente de nuevo" << endl;
 				}
 				else{
-					//blank
+					cout << "opcion invalida, intente de nuevo" << endl;
 				}
 		}
-		
-	
+
+
 	} while(choice !=0);
 	cout << "Fin del programa" << endl;
 	return 0;
